d4d09c1657d795d8686f6400cb1ac960de725ec3
rimjhimittal <rmittal_be21@thapar.edu>
2022-09-14 15:53:39 +0530

    printed length of LL

--- LinkedLists/DoublyLL.cpp
+++ LinkedLists/DoublyLL.cpp
@@ -28,10 +28,22 @@ void print(Node * &head){
 
 }
 
+int getLen(Node * &head){
+    int len = 0;
+    Node * temp = head;
+    while(temp!= NULL){
+        len++;
+        temp= temp-> next;
+    }
+    return len;
+}
+
 int main(){
     Node * node1 = new Node(10);
     Node * head = node1;
     print(head);
+    getLen(head);
+    cout<<endl<<"The length of your Doubly Linked List is "<<getLen(head)<<endl;
 
 
 
