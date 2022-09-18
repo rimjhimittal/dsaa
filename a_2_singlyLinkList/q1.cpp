c7156a0157c0059583e515058bae8c2a20809a09
rimjhimittal <rmittal_be21@thapar.edu>
2022-09-18 15:57:24 +0530

    q1 done

--- a_2_singlyLinkList/q1.cpp
+++ a_2_singlyLinkList/q1.cpp
@@ -10,7 +10,6 @@ class Node{
     Node(int data){
         this->data= data;
         this->next= NULL;
-
     }
 
     //destructor
@@ -102,6 +101,20 @@ void deletenode(Node * &head, Node * &tail, int position){
 
 }
 
+int search(int d, Node* &head){
+    Node * temp = head;
+    int pos= 1;
+    
+    while(temp!= NULL){
+        if(temp->data==d){
+            cout<< "position:"<<pos<<endl;;
+        }
+        temp= temp-> next;
+        pos++;
+    }
+}
+
+
 void print(Node * &head){
 
     Node * temp = head;
@@ -118,15 +131,48 @@ int main(){
     Node * node1 = new Node(10);
     Node * head = node1;
     Node * tail = node1;
-    print(head);
     insertAtHead(head, 15);
-    print(head);
     insertAtTail(tail, 12);
-    print(head);
     insertAtPosition(2, head, tail, 56);
+    cout<<"The given linked list is:";
     print(head);
-    deletenode(head , tail,  4);
-    print(head);
-    cout<<"head "<<head-> data<<endl;
-    cout<<"tail "<<tail-> data<<endl;
+    cout<<"What operations would you like to perform\n1. Insertion at the beginning\n2. Insertion At the end.\n3. Insert at Position.\n4. Delete from beginning\n5. Deletion from the end.\n6. Delete a specific node.\n7. Search for a anode and siplay its position.\n8. Display all node values."<<endl;
+    int  n, d, pos;
+    cin>>n;
+    switch(n){
+        case 1: cout<<"What element would you like to insert at head?"<<endl;
+                cin>>d;
+                insertAtHead(head,d);
+                break;
+        case 2: cout<<"What element would you like to insert at the end?"<<endl;
+                cin>>d;
+                insertAtTail(tail,d);        
+                break;
+        case 3: cout<<"What element would you like to insert?"<<endl;
+                cin>>d;
+                cout<<"Position?"<<endl;
+                cin>>pos;
+                insertAtPosition(pos, head, tail, d);  
+                break;      
+        case 4: deletenode(head, tail, 1);
+                break;
+        case 5: deletenode(head, tail, 4);
+                break;
+        case 6: cout<<"Position?"<<endl;
+                cin>>pos;
+                deletenode(head, tail , pos);
+                break;
+        case 7: cout<<"Element?"<<endl;
+                cin>>d;
+                search(d, head);
+                break;
+        case 8: 
+                break;
+        default: print(head);
+        }
+
+        print(head);
+
+
+
 }
